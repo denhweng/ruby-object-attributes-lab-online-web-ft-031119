d0c16349396144eb54c835c15f790b91070b7f93
Dan Hwang <danhwangfkpc@gmail.com>
2019-03-24 00:52:33 +0000

    Automatically backed up by Learn

--- lib/person.rb
+++ lib/person.rb
@@ -4,4 +4,16 @@ class Person
     @name = person_name 
   end
   
-  def name 
+  def name
+    @name 
+  end 
+  
+  def job=(person_job)
+    @job = person_job 
+  end
+  
+  def job 
+    @job
+  end
+
+end
